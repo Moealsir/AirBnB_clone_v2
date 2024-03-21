b8b106b71e6973e60a8b4d043879395c486f180b
Kirito99x <msaifeldin46@gmail.com>
2024-03-21 03:34:38 +0200

    fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
@@ -8,8 +15,13 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
-        """Returns a dictionary of models currently in storage"""
+    def all(self, cls=None):
+        """(UPdated)Returns a dictionary of models currently in storage"""
+        if isinstance(cls,str):
+            cls = globals().get(cls)
+        if cls and issubclass(cls, BaseModel):
+            dict_for_cls = {key: value for key, value in self.__objects.items() if isinstance(value, cls)}
+            return dict_for_cls
         return FileStorage.__objects
 
     def new(self, obj):
@@ -48,3 +60,10 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+
+    def delete(self, obj=None):
+        """Delete obj from __objects if it's inside"""
+        if obj is not None:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            self.__objects.pop(key, None)
