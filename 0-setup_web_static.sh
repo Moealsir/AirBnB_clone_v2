bea7ad843b2a1f52132bba0e38bd2c86546ee6ca
Kirito99x <msaifeldin46@gmail.com>
2024-04-03 22:45:42 +0200

    web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -26,7 +26,7 @@ printf %s "server {
     }
 
     location /redirect_me {
-        return 301 http://cuberule.com/;
+        return 301 http://kirito99x.tech;
     }
 
     error_page 404 /404.html;
