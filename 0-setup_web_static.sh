ad589d1cbe30c7ed6af13c36754851b998f2ec51
Kirito99x <msaifeldin46@gmail.com>
2024-04-03 22:12:49 +0200

    web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -13,5 +13,27 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu /data/
 sudo chgrp -R ubuntu /data/
 
-sudo sed -i '38i\ \ location /hbnb_static/ {\n \ \ \ \ alias /data/web_static/current/;\n \ \ }\n' /etc/nginx/sites-available/default
-sudo service nginx start
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
+
+service nginx restart
