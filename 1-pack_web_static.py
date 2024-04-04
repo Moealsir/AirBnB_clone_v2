2b2d5bf1b7abb275d0c23a9cd82590722bcfbd05
Kirito99x <msaifeldin46@gmail.com>
2024-04-04 02:54:03 +0200

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,11 +7,11 @@ from datetime import datetime
 
 def do_pack():
     """create a .tgs archive from the contents of the web_static folder"""
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_file = "versions/web_static_{}.tgz".format(date)
     try:
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
         if os.path.isdir("versions") is False:
             local("mkdir versions")
-        archive_file = "versions/web_static_{}.tgz".format(date)
         local("tar -cvzf {} web_static".format(archive_file))
         return archive_file
     except Exception as e:
