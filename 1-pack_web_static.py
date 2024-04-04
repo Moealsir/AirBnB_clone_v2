0e4f1346dec5e9880fa0b33da9f7978825d92f10
Kirito99x <msaifeldin46@gmail.com>
2024-04-04 02:16:42 +0200

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""a module  that generates a .tgz archives"""
+"""a module that generates a .tgz archives from the web_statics folder"""
 import os.path
 from fabric.api import local
 from datetime import datetime
@@ -8,7 +8,12 @@ from datetime import datetime
 def do_pack():
     """create a .tgs archive from the contents of the web_static folder"""
     date = datetime.utcnow()
-    archive_file = "versions/web_static_{}{}{}{}{}{}.tgz".format(date.year, date.month, date.day, date.hour, date.minute, date.second)
+    archive_file = "versions/web_static_{}{}{}{}{}{}.tgz".format(date.year,
+                                                                 date.month,
+                                                                 date.day,
+                                                                 date.hour,
+                                                                 date.minute,
+                                                                 date.second)
 
     if os.path.isdir("versions") is not True:
         if local("mkdir -p versions").failed is True:
