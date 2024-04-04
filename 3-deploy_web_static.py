08b430a55c7dc7be9c10d60fd3d7f047e26a48c6
Kirito99x <msaifeldin46@gmail.com>
2024-04-04 22:45:51 +0200

    3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -18,24 +18,3 @@ def do_pack():
         return archive_file
     except Exception as e:
         return None
-
-
-def deploy(archive_path):
-    """distributes an archive to your web servers using function deploy"""
-    if os.path.exists(archive_path) is False:
-        return False
-    folder = archive_path.spilt("/")[-1]
-    file_name = folder.spilt(".")[0]
-    try:
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, file_name))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(folder, path, file_name))
-        run('rm /tmp/{}'.format(folder))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, file_name))
-        run('rm -rf {}{}/web_static'.format(path, file_name))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, file_name))
-        return True
-    except Exception as e:
-        return False
