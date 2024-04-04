0cfcc65578a42acea23bb2612a5a82c1e8416569
Kirito99x <msaifeldin46@gmail.com>
2024-04-05 00:31:42 +0200

    3-deploy_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,1 +1,68 @@
+
 #!/usr/bin/python3
+"""a module that istributes an archive to your web servers,
+using the function do_deploy
+"""
+from fabric.api import local, put, run, env
+from datetime import datetime
+from os.path import exists, isdir
+import os
+env.hosts = ['18.235.255.77', '54.160.73.198']
+
+
+def do_pack():
+    """create a .tgs archive from the contents of the web_static folder"""
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_file = "versions/web_static_{}.tgz".format(date)
+    try:
+        if isdir("versions") is False:
+            local("mkdir versions")
+        local("tar -cvzf {} web_static".format(archive_file))
+        return archive_file
+    except Exception as e:
+        return None
+
+
+def do_deploy(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
+        return False
+    try:
+        folder = archive_path.split("/")[-1]
+        file_name = folder.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, file_name))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(folder, path, file_name))
+        run('rm /tmp/{}'.format(folder))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, file_name))
+        run('rm -rf {}{}/web_static'.format(path, file_name))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, file_name))
+        return True
+    except:
+        return False
+
+
+def deploy():
+    """creates & distributes an archive to servers"""
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+    return do_deploy(archive_path)
+
+
+def do_clean(number=0):
+    """Delete outdated archives."""
+    number = 1 if int(number) == 0 else int(number)
+
+    archive = sorted(os.listdir("versions"))
+    [archive.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archive]
+
+    with cd("/data/web_static/releases"):
+        archive = run("ls -tr").split()
+        archive = [a for a in archive if "web_static_" in a]
+        [archive.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archive]
