b5b16ea9d7f708f15c13d8d104141a0e953fc192
Kirito99x <msaifeldin46@gmail.com>
2024-04-04 22:48:54 +0200

    3-deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,27 +2,27 @@
 """a module that istributes an archive to your web servers,
 using the function do_deploy
 """
-import os.path
+from  os.path import exists
 from fabric.api import env, put, run
 env.hosts = ['18.235.255.77', '54.160.73.198']
 
 
 def do_deploy(archive_path):
-    """distributes an archive to your web servers using function deploy"""
-    if os.path.exists(archive_path) is False:
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
     try:
-        folder = archive_path.spilt("/")[-1]
-        file_name = folder.spilt(".")[0]
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, file_name))
-        run('tar -xzf /tmp/{}.tgz -C {}{}/'.format(folder, path, file_name))
-        run('rm /tmp/{}.tgz'.format(folder))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, file_name))
-        run('rm -rf {}{}/web_static'.format(path, file_name))
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, file_name))
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
     except:
         return False
