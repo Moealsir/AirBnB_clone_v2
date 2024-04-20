e839c5f9b3d34ee529969908ef0737366f6d40b3
Moealsir <mohamedwdalsir@gmail.com>
2024-04-20 22:04:47 +0300

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,9 +5,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Diplay hbnb"""
+    """Displays 'Hello HBNB!'"""
     return 'Hello HBNB!'
 
 
