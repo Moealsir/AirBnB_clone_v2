91a159e58572bbfc952968f7b21dc8e38eb93092
Moealsir <mohamedwdalsir@gmail.com>
2024-04-20 22:20:58 +0300

    2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -19,9 +19,9 @@ def hbnb():
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def cisfun(text):
-    """display “C ” followed by the text"""
-    return 'C ' + text.replace('_', ' ')
+def c_text(text):
+    """c_text"""
+    return ('C {}'.format(text.replace('_', ' ')))
 
 
 if __name__ == '__main__':
