ba52e84cf4d8ff0d9a65e73b9326a75ed317d3b0
Moealsir <mohamedwdalsir@gmail.com>
2024-04-20 22:40:00 +0300

    3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""0-hello_route module"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Display HBNB!"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """c_text"""
+    return ('C {}'.format(text.replace('_', ' ')))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is_cool'):
+    """python_text"""
+    return ('Python {}'.format(text.replace('_', ' ')))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
